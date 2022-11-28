bd0a8d8836a92c0020c408a73a2cd3cb86d85a5a
Tobias Persson <tobiaspn@axis.com>
2022-11-28 13:51:04 +0100

    Update to intermediary pyscaffold 3.2.3

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Setup file for eiffellib.
-
-This file was generated with PyScaffold 2.5, a tool that easily
-puts up a scaffold for your new Python project. Learn more under:
-http://pyscaffold.readthedocs.org/
-"""
-
+"""Setup file for eiffellib."""
 import sys
+
+from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx', 'm2r'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
 
 if __name__ == "__main__":
-    setup_package()
+    setup(use_psycaffold=True)
