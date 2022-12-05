188edfd4517244a00de30151f72db095a7b70b89
Tobias Persson <tobiaspn@axis.com>
2022-12-05 13:03:05 +0100

    Update pyscaffold (#52)
    * Update pyscaffold version

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Setup file for eiffellib.
-
-This file was generated with PyScaffold 2.5, a tool that easily
-puts up a scaffold for your new Python project. Learn more under:
-http://pyscaffold.readthedocs.org/
-"""
-
-import sys
+"""Setup file for eiffellib."""
 from setuptools import setup
 
-
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx', 'm2r'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
-
-
 if __name__ == "__main__":
-    setup_package()
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
